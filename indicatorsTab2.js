794dd28dbac44c74177238fa0aefbf0e0d79bcb2
BurcuUnluturk <62424609+BurcuUnluturk@users.noreply.github.com>
2020-12-18 11:42:01 +0100

    Add files via upload

---
+++ indicatorsTab2.js
@@ -0,0 +1,9 @@
+export default 
+ 	 [ 
+		"Import/Export of biofuels",
+		"Primary biomass supply",
+		"RE refineries input",
+		"RE refineries output",
+		"RE_refineries_by_type",
+		"Supply annual system costs",
+	 ]
---
+++ indicatorsTab7.js
@@ -0,0 +1,9 @@
+export default 
+ 	 [ 
+		"Freight transport demand",
+		"Fuel consumption cars",
+		"Fuel consumption transport",
+		"Passenger transport demand",
+		"Transport CO2 emissioner",
+		"Transport System Cost",
+	 ]
