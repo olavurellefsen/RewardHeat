9d7d1c6572ef049d549464b7f1ae9e9ae3cf9be1
BurcuUnluturk <62424609+BurcuUnluturk@users.noreply.github.com>
2021-02-09 13:47:15 +0100

    BY data fix
    BY data is fixed to Transition-WEO-NP scenario for all countries

--- indicatorsTab1.js
+++ indicatorsTab1.js
@@ -1,9 +1,12 @@
 export default 
  	 [ 
+		"Annual system cost (MEUR)",
 		"District heat capacity (GW)",
 		"District heat production (PJ)",
 		"End-use devices capacity (GW)",
 		"End-use heat demand (PJ)",
 		"Excess heat use (PJ)",
 		"Primary energy use (PJ)",
+		"Total  air pollution (kt)",
+		"Urban excess heat use (PJ)",
 	 ]
