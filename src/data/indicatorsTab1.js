0f4fd969ef4ee073a8d27f67be8b0e5f6294b7d6
BurcuUnluturk <62424609+BurcuUnluturk@users.noreply.github.com>
2021-03-10 09:33:10 +0100

    Graph order change

--- src/data/indicatorsTab1.js
+++ src/data/indicatorsTab1.js
@@ -1,10 +1,10 @@
 export default 
  	 [ 
-		"Annual system cost (MEUR)",
-		"District heat capacity (GW)",
-		"District heat production (PJ)",
 		"End-use devices capacity (GW)",
 		"End-use heat demand (PJ)",
+		"District heat capacity (GW)",
+		"District heat production (PJ)",
 		"All excess heat use (PJ)",
 		"Low temperature heat use (PJ)",
+		"Annual system cost (MEUR)",
 	 ]
